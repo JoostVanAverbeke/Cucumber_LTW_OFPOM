7329347ffcab1bfffd77397082715178c639db11
Joost Van Averbeke <Joost.VanAverbeke@mips.be>
2022-10-28 12:16:00 +0200

    Added appserver based delete of instance data

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,3 +9,15 @@ require 'silvio_glims'
 require 'silvio_hl7_factory'
 
 FactoryGirl.find_definitions
+
+appserver = SilvioCore::Appserver.new
+appserver.authenticateWith('sysman', 'mansys', true)
+
+result=appserver.clearInstanceData
+
+if result.nil?
+  puts 'Clear the instance data on the appserver'
+else
+  puts "Clear the instance data on the appserver (Glims version: #{result['GlimsVersion']})"
+end
+
