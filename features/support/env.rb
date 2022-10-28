b7bbcb57dc52267a85810429e8dbcf6514732012
Joost Van Averbeke <Joost.VanAverbeke@mips.be>
2022-10-28 11:33:32 +0200

    added require of 'silvio_hl7_factory' in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,5 +6,6 @@ $application = SilvioCore::ApplicationFactory.new.create
 SilvioCore.require_openedge_jar($application.openedge_version.to_s)
 
 require 'silvio_glims'
+require 'silvio_hl7_factory'
 
 FactoryGirl.find_definitions
